3bb621bb78e54e4a781663910f0d20bb9759e456
emperatriz108 <emperatriz108@gmail.com>
2022-10-28 22:54:02 -0600

    Interfaz de login

---
+++ Vistas/login.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Assets/css/login.css">
+    <title>Login</title>
+</head>
+<body class="alineacion">
+    <div class="formulario">
+        Hola Mundo
+    </div>
+</body>
+</html>
