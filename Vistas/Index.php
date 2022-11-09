0ae90efe9f5e38875969c51541154189e72ec523
NelsonVasquezues <NelsonVasquezues>
2022-11-09 15:17:12 -0600

    updated

--- Vistas/Index.php
+++ Vistas/Index.php
@@ -1,60 +1,52 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <title>Inicio</title>
-    <?php include './inc/link.php'; ?>
-</head>
-<body id="container-page-index">
-    <?php include './inc/navbar.php'; ?>
-    <div class="jumbotron" id="jumbotron-index">
-      <h1><span class="tittles-pages-logo">Asociación de Futbol Aficionado </span> <small style="color: #fff;"></small></h1>
-      <p>
-          Bienvenido a ADFA en linea
-      </p>
-    </div>
-    <section id="new-prod-index">
-         <div class="container">
-            <div class="page-header">
-                <h1>Novedades <small>productos</small></h1>
-            </div>
-            <div class="row">  
-            </div>
-         </div>
-    </section>
-    <section id="reg-info-index">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 text-center">
-                   <article style="margin-top:20%;">
-                        <p><i class="fa fa-users fa-4x"></i></p>
-                        <h3>Registrate</h3>
-                        <p>Registrese en <span class="tittles-pages-logo">ADFA en linea</span> para recibir todas las notificaciones de los partidos de nuestra asociación.</p>
-                        <p><a href="registration.php" class="btn btn-info btn-block">Registrarse</a></p>   
-                   </article>
-                </div>
-                <div class="col-xs-12 col-sm-6">
-                    <img src="assets/img/img-registration.png"  class="img-responsive">
-                </div>
-            </div>
-        </div>
-    </section>
-    <section id="distribuidores-index">
-        <div class="container">
-            <div class="col-xs-12 col-sm-6">
-
-            </div>
-            <div class="col-xs-12 col-sm-6">
-
-            </div>
-            <div class="col-xs-12">
-                <div class="page-header">
-                  <h1><small style="color: #333;"> Nuestras divisiones</small></h1>
-                </div>
-                <br><br>
-                <img src="assets/img/marcas.jpg" alt="logos-marcas" class="img-responsive">
-            </div>
-        </div>
-    </section>
-    <?php include './inc/footer.php'; ?>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title><?php echo $data["titulo"]; ?></title>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<script src="assets/js/bootstrap.min.js" ></script>
+	</head>
+	
+	<body>
+		<div class="container">
+			<h2><?php echo $data["titulo"]; ?></h2>
+			
+			<a href="index.php?c=vehiculos&a=nuevo" class="btn btn-primary">Agregar</a>
+			
+			<br />
+			<br />
+			<div class="table-responsive">
+				<table border="1" width="80%" class="table">
+					<thead>
+						<tr class="table-primary">
+							<th>Placa</th>
+							<th>Marca</th>
+							<th>Modelo</th>
+							<th>Año</th>
+							<th>Color</th>
+							<th>Editar</th>
+							<th>Eliminar</th>
+						</tr>
+					</thead>
+					
+					<tbody>
+						<?php foreach($data["vehiculos"] as $dato) {
+							echo "<tr>";
+							echo "<td>".$dato["placa"]."</td>";
+							echo "<td>".$dato["marca"]."</td>";
+							echo "<td>".$dato["modelo"]."</td>";
+							echo "<td>".$dato["anio"]."</td>";
+							echo "<td>".$dato["color"]."</td>";
+							echo "<td><a href='index.php?c=vehiculos&a=modificar&id=".$dato["id"]."' class='btn btn-warning'>Modificar</a></td>";
+							echo "<td><a href='index.php?c=vehiculos&a=eliminar&id=".$dato["id"]."' class='btn btn-danger'>Eliminar</a></td>";
+							echo "</tr>";
+						}
+						?>
+					</tbody>
+					
+				</table>
+			</div>
+		</div>
+	</body>
 </html>
