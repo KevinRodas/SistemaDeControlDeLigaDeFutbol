444dd3fc5d540092e24c7041f42cead487c209e7
KevinRodas <krodas391@gmail.com>
2022-11-16 23:21:32 -0600

    commit

---
+++ Controladores/Administrativo.php
@@ -0,0 +1,22 @@
+<?php
+class Jugador{
+    public $codigo;
+    public $nombre;
+    public $apellido;
+    public $edad;
+    public $ntelefono;
+    public $correo;
+    public $puesto;
+    
+    public function Cancelacion_Sanciones(){
+
+    }
+    
+    public function Crear_Partido(){
+
+    }
+    
+    public function Registrar_Equipo(){
+
+    }
+}
