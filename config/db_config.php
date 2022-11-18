92bfb1a94e45bf622ffba8ff4cc2ab1675f83bc5
emperatriz108 <emperatriz108@gmail.com>
2022-11-18 04:53:18 -0600

    se manejo el control de las direcciones

---
+++ config/db_config.php
@@ -0,0 +1,18 @@
+<?php
+//Definicion de constantes para conexion a la base de datos
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','bd_sistema_futbol');
+define('DB_CHARSET','utf8');
+
+define('T_USUARIO','tbl_usuario');
+define('U_ID','id_usuario');
+define('U_NOM','nombre');
+define('U_LN','apellido');
+define('U_AGE','edad');
+define('U_TEL', 'telefono');
+define('U_MAIL', 'correo');
+define('U_PASS', 'password');
+define('U_ROL','rol');
+?>
