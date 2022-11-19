5f62f7876177cb13bc9864258080ad9f5fb15552
KevinRodas <krodas391@gmail.com>
2022-11-18 19:36:44 -0500

    commit

---
+++ Controladores/Reportes.php
@@ -0,0 +1,32 @@
+<?php
+class Jugador{
+    public $cod_reporte;
+    public $cod_partido;
+    public $ntarjetas;
+    public $cod_detalle_sanciones;
+    public $observaciones;
+    
+    public function Crear_Reporte(){
+
+    }
+    
+    public function Actualizar_Reporte(){
+
+    }
+    
+    public function Ver_Reporte(){
+
+    }
+
+    public function Buscar_Reporte(){
+
+    }
+
+    public function Eliminar_Reporte(){
+
+    }
+
+    public function Crear_Detalle_Sanciones(){
+
+    }
+}
