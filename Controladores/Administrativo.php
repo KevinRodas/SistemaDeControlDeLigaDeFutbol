eac8345b6433e9a9306309c5c00c06b49d519ab2
KevinRodas <krodas391@gmail.com>
2022-11-18 20:25:58 -0500

    commit

--- Controladores/Administrativo.php
+++ Controladores/Administrativo.php
@@ -1,5 +1,5 @@
 <?php
-class Jugador{
+class Administrativo{
     public $codigo;
     public $nombre;
     public $apellido;
@@ -17,6 +17,110 @@ class Jugador{
     }
     
     public function Registrar_Equipo(){
+
+    }
+
+    public function Mostrar_Lista_Report_Partido(){
+
+    }
+
+    public function Ver_Reporte(){
+
+    }
+
+    public function Editar_Reporte(){
+
+    }
+
+    public function Eliminar_Reporte(){
+
+    }
+
+    public function Buscar_Reporte(){
+
+    }
+
+    public function Mostar_Lista_Jugadores(){
+
+    }
+
+    public function Ver_Jugador(){
+
+    }
+
+    public function Editar_Jugador(){
+
+    }
+
+    public function Eliminar_Jugador(){
+
+    }
+
+    public function Buscar_Jugador(){
+
+    }
+
+    public function Mostar_Lista_Equipos(){
+
+    }
+
+    public function Ver_Equipo(){
+
+    }
+
+    public function Editar_Equipo(){
+
+    }
+
+    public function Eliminar_Equipo(){
+
+    }
+
+    public function Buscar_Equipo(){
+
+    }
+
+    public function Mostrar_Lista_Arbitros(){
+
+    }
+
+    public function Ver_Arbitro(){
+
+    }
+
+    public function Editar_Arbitro(){
+
+    }
+
+    public function Eliminar_Arbitro(){
+
+    }
+
+    public function Buscar_Arbitro(){
+
+    }
+
+    public function Mostrar_Lista_Representantes(){
+
+    }
+
+    public function Ver_Representante(){
+
+    }
+
+    public function Editar_Representante(){
+
+    }
+
+    public function Eliminar_Representante(){
+
+    }
+
+    public function Buscar_Representante(){
+
+    }
+
+    public function Mostar_Lista_Partidos(){
 
     }
 }
