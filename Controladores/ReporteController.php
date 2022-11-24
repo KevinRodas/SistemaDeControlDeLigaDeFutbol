995549598c2ffe9e9137c26008f851238490a68f
KevinRodas <krodas391@gmail.com>
2022-11-24 04:10:22 -0500

    commit

--- Controladores/ReporteController.php
+++ Controladores/ReporteController.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
  require_once "config/configGeneral.php";
  require_once "config/db_config.php";
@@ -65,72 +65,4 @@ class ReporteController
 
 }
 
-=======
-<?php
- require_once "config/configGeneral.php";
- require_once "config/db_config.php";
- require_once "Modelos/Reportes.php";
-class ReporteController 
-{
-    public function crearReporte(){
-        if(!empty($_POST)){
-           
-            $r= new Reportes();
-            $r->setIdReporte($_POST[REPORT_ID]);
-            $r->setIdPartido($_POST[REPORT_PART]);
-            $r->setNumTarjetas($_POST[REPORT_TARJ]);
-            $r->setObservaciones($_POST[REPORT_OBSERV]);
-
-            if($r->Crear_Reporte()){
-                header("Location:".BASE_DIR.'/PanelArbitro/showAdminRepote');
-            }
-            else{
-                header("Location:".BASE_DIR.'Reportes/showcreateReporte' );
-            }
-            
-
-        }
-        else{
-            echo "No hay datos";
-        }
-
-    }
-
-    public function showcreateReporte(){
-        require_once "Vistas/CrearReporte.php";
-        
-   }
-   public function showReportes(){
-        $Reporte = new Reportes(); //Creamos una instancia de la clase Torneo
-
-        $registros = $Reporte->Ver_Reportes(); //Pedimos la lista de torneos
-        $data[T_REPORTE] = "";
-
-        if ($registros != null) {
-            $data[T_REPORTE] = $registros;
-            
-        }
-
-        require_once "Vistas/";
-    
-    }
-
-  public function buscarDireccion($action){
-    if ($action=='showcreateReporte') {
-           $this->showcreateReporte();
-    }
-    elseif ($action=="showReportes") {
-        $this->showReportes();
-    }
-    elseif ($action=="crearReporte") {
-        $this->crearReporte();
-    }
-    else{
-        return false;
-    }
- }
-
-}
-
->>>>>>> 05c26e1b45488b9a6badd5ef42a2fcbf06b2cb1e
 ?>
--- Controladores/SancionesController.php
+++ Controladores/SancionesController.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
  require_once "config/configGeneral.php";
  require_once "config/db_config.php";
@@ -48,55 +48,5 @@ class SancionesController
 
 }
 
-=======
-<?php
- require_once "config/configGeneral.php";
- require_once "config/db_config.php";
- require_once "Modelos/Sanciones.php";
-class SancionesController 
-{
-    public function createSancion(){
-        if(!empty($_POST)){
-            
-            $s= new Sanciones();
-            $s->setCodPartido($_POST[SANCION_PART]);
-            $s->setCodArbitro($_POST[SANCION_ARB]);
-            $s->setCodSancionado($_POST[SANCION_ID_SAN]);
-            $s->setCategoria($_POST[SANCION_CAT]);
-            $s->setFecha_Sancion($_POST[SANCION_INICIO]);
-            $s->setFecha_Fin($_POST[SANCION_FIN]);
-            $s->setDias_Penalizacion($_POST[SANCION_DIA]);
-            $s->setPrecio($_POST[SANCION_PR]);
-            $s->setEstado($_POST[SANCION_EST]);
 
-            if($s->Crear_Sancion()){
-                header("Location:".BASE_DIR.'/ ');
-            }
-            else{
-                header("Location:".BASE_DIR.'Sanciones/' );
-            }
-        }
-        else{
-            echo "No hay datos";
-        }
-
-    }
-
-  
-
-    public function showcrearSancion(){
-        require_once "Vistas/";
-    }
-
-    public function showUpdate(){
-        require_once "Vistas/";
-    }
-
-    
-
-   
-
-}
-
->>>>>>> 05c26e1b45488b9a6badd5ef42a2fcbf06b2cb1e
 ?>
