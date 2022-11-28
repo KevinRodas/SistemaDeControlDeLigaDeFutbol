9d01d91885e93df0930052c13fd2b0c63d210a09
emperatriz108 <emperatriz108@gmail.com>
2022-11-28 07:26:09 -0600

    Configuracion del entorno de reporte

--- Controladores/ReporteController.php
+++ Controladores/ReporteController.php
@@ -2,42 +2,161 @@
  require_once "config/configGeneral.php";
  require_once "config/db_config.php";
  require_once "Modelos/Reportes.php";
+ require_once "Modelos/Sanciones.php";
+
 class ReporteController 
 {
+
+    public function generarSanciones($id_sancionado,$id_partido,$id_arbitro,$categoria,$dias,$precio,$id_reporte,$descripcion){
+        $sancion= new Sanciones();
+
+        $fecha=date("d").'-'. date("m") .'-'.date("Y");
+        echo "la fecha actual es " . $fecha;
+        $fecha_actual = date("Y-m-d");
+        //sumo 1 día
+        $fecha_final= date("Y-m-d",strtotime($fecha_actual."+ ".$dias. " days")); 
+        //resto 1 día
+       // echo date("d-m-Y",strtotime($fecha_actual."- 1 days")); 
+
+        $sancion->setCodSancionado($id_sancionado);
+        $sancion->setCodPartido($id_partido);
+        $sancion->setCodArbitro($id_arbitro);
+        $sancion->setCategoria($categoria);
+        $sancion->setDias_Penalizacion($dias);
+        $sancion->setFecha_Sancion($fecha_actual);
+        $sancion->setFecha_Fin($fecha_final);
+        $sancion->setEstado('Pendiente');
+        $sancion->setPrecio($precio);
+        $sancion->setIdReporte($id_reporte);
+        $sancion->setDescripcion($descripcion);
+
+        $sancion->Crear_Sancion();
+
+
+   }
+
     public function crearReporte(){
         if(!empty($_POST)){
            
             if(!empty($_POST[REPORT_ID])){
                 echo " funciono<br>";
-                echo  $_POST[REPORT_ID]."<br>";
-                echo  $_POST[ARB_ID]."<br>";
-                echo "-------------------------<br>";
-                echo $_POST["e1-id1"]."<br>";
-                echo $_POST['e1-amarilla1']."<br>";
-                echo $_POST['e1-roja1']."<br>";
-                echo $_POST['e1-motv1']."<br>";
-                echo $_POST['e1-dias1']."<br>";
-                echo $_POST['e1-precio1']."<br>";
-                echo "-------------------------<br>";
-                echo $_POST['e1-id2']."<br>";
-                echo $_POST['e1-amarilla2']."<br>";
-                echo $_POST['e1-roja2']."<br>";
-                echo $_POST['e1-motv2']."<br>";
-                echo $_POST['e1-dias2']."<br>";
-                echo $_POST['e1-precio2']."<br>";
+                echo $_POST[REPORT_ID]."<br>";
+                echo $_POST[ARB_ID]."<br>";
+                echo $_POST[PART_ID]."<br>";
 
                 $r= new Reportes();
                 $r->setIdReporte($_POST[REPORT_ID]);
                 $r->setIdPartido($_POST[REPORT_PART]);
-                $r->setNumTarjetas($_POST[REPORT_TARJ]);
+                $r->setNumTarjetas(0);
                 $r->setObservaciones('');
+                $r->Crear_Reporte();
+
+                echo "-------------------------<br>";
+                $tarjetasE1 = 0;
+                $tarjetasE2 = 0;
+
+                if(isset($_POST["datos1"]) && !empty($_POST["datos1"])){
+                        
+                    $rows= $_POST["datos1"];
+
+                    for ($i=1; $i <= $rows; $i++) { 
+                        $cadenaId ="e1-id".$i;
+                        $cadenaAmarilla ="e1-amarilla".$i;
+                        $cadenaRoja ="e1-roja".$i;
+                        $cadenaMotv ="e1-motv".$i;
+                        $cadenaDias ="e1-dias".$i;
+                        $cadenaPrecio ="e1-precio".$i;
+
+                        if(isset($_POST[$cadenaId])){
+
+                            if(!empty($_POST[$cadenaAmarilla]) && !empty($_POST[$cadenaRoja]) && !empty($_POST[$cadenaMotv]) && !empty($_POST[$cadenaDias]) && !empty($_POST[$cadenaPrecio])){
+                                $categoria = "";
+                                echo "----JUGADOR".$i." EQUIPO 1------<br>";
+                                echo $_POST[$cadenaId]."<br>";
+                                echo $_POST[$cadenaAmarilla]."<br>";
+                                echo $_POST[$cadenaRoja]."<br>";
+                                echo $_POST[$cadenaMotv]."<br>";
+                                echo $_POST[$cadenaDias]."<br>";
+                                echo $_POST[$cadenaPrecio]."<br>";
+
+                                if($_POST[$cadenaAmarilla] == 0 && $_POST[$cadenaRoja] == 0){
+                                    echo "----NO hay sancion------<br>";
+                                }
+                                else{
+                                    if($_POST[$cadenaAmarilla] >= 1 && $_POST[$cadenaRoja] == 0){
+                                        $categoria = "Leve";
+                                    }
+                                    elseif ($_POST[$cadenaAmarilla] < 1 && $_POST[$cadenaRoja]>1) {
+                                        $categoria = "Grave";
+                                    }
+                                    elseif ($_POST[$cadenaAmarilla] >= 1 && $_POST[$cadenaRoja]>=1) {
+                                        $categoria = "Grave";
+                                    }
+    
+                                    $this->generarSanciones($_POST[$cadenaId],$_POST[PART_ID],$_POST[ARB_ID],$categoria,$_POST[$cadenaDias],$_POST[$cadenaPrecio],$_POST[REPORT_ID],$_POST[$cadenaMotv]);
+    
+                                }
+                                $tarjetasE1 = $tarjetasE1 + $_POST[$cadenaAmarilla] + $_POST[$cadenaRoja];
+
+                            }
+                            else{
+                                echo "----NO LLENARON LOS CAMPOS------<br>";
+                            }
+                        }
+                        else{
+                            echo "----NO HAY JUGADORES EN EL EQUIPO 1------<br>";
+                        }
+                    }
+                }
+
+                //CONDICIONES DE SANCIONES 2
+                if(isset($_POST["datos2"]) && !empty($_POST["datos2"])){
+                        
+                    $rows= $_POST["datos2"];
+
+                    for ($i=1; $i <= $rows; $i++) { 
+                        $cadenaId ="e2-id".$i;
+                        $cadenaAmarilla ="e2-amarilla".$i;
+                        $cadenaRoja ="e2-roja".$i;
+                        $cadenaMotv ="e2-motv".$i;
+                        $cadenaDias ="e2-dias".$i;
+                        $cadenaPrecio ="e2-precio".$i;
+
+                        if(isset($_POST[$cadenaId])){
+
+                            if(!empty($_POST[$cadenaAmarilla]) && !empty($_POST[$cadenaRoja]) && !empty($_POST[$cadenaMotv]) && !empty($_POST[$cadenaDias]) && !empty($_POST[$cadenaPrecio])){
+                                echo "----JUGADOR".$i." EQUIPO 1------<br>";
+                                echo $_POST[$cadenaId]."<br>";
+                                echo $_POST[$cadenaAmarilla]."<br>";
+                                echo $_POST[$cadenaRoja]."<br>";
+                                echo $_POST[$cadenaMotv]."<br>";
+                                echo $_POST[$cadenaDias]."<br>";
+                                echo $_POST[$cadenaPrecio]."<br>";
+                                $tarjetasE2 = $tarjetasE2 + $_POST[$cadenaAmarilla] + $_POST[$cadenaRoja];
+                            }
+                            else{
+                                echo "----NO LLENARON LOS CAMPOS------<br>";
+                            }
+                        }
+                        else{
+                            echo "----NO HAY JUGADORES EN EL EQUIPO 2------<br>";
+                        }
+                    }
+                }
+                /////
+                
+                echo "Tarjetas E1: ".$tarjetasE1;
+                echo "Tarjetas E2: ".$tarjetasE2;
+                $totalTarjetas = $tarjetasE1 + $tarjetasE2;
+
+                $rm= new Reportes();
+                $rm->setIdReporte($_POST[REPORT_ID]);
+                $rm->setNumTarjetas($totalTarjetas);
+                $rm-> Actualizar_Reporte_NumTarjetas();
+                
+
             }
-           //echo $_POST[REPORT_ID];
-            $r= new Reportes();
-            //$r->setIdReporte($_POST[REPORT_ID]);
-            //$r->setIdPartido($_POST[REPORT_PART]);
-            //$r->setNumTarjetas($_POST[REPORT_TARJ]);
-            //$r->setObservaciones($_POST[REPORT_OBSERV]);
+           
             require_once "Vistas/prueba.php";
 /*
             if($r->Crear_Reporte()){
@@ -88,6 +207,7 @@ class ReporteController
                 $dato2='';
                 foreach ($data[T_PARTIDO] as $dato) {
                     $nombrepartido = $dato[PART_NOM];
+                    $IDpartido = $dato[PART_ID];
                     $idArb= $dato[PART_ARB];
                     $dato1=$equipo1->setID(strval($dato[PART_EQP1]));
                     $dato2=$equipo2->setID(strval($dato[PART_EQP2]));
@@ -171,30 +291,7 @@ class ReporteController
     }      
    }
    
-   public function generarSanciones($id_sancionado,$id_partido,$id_arbitro,$categoria,$dias,$precio){
-        require_once "Modelos/Sanciones.php";
-        $sancion= new Sanciones();
 
-        $fecha_actual = date("d-m-Y");
-        //sumo 1 día
-        $fecha_final= date("d-m-Y",strtotime($fecha_actual."+".$dias. " days")); 
-        //resto 1 día
-       // echo date("d-m-Y",strtotime($fecha_actual."- 1 days")); 
-
-        $sancion->setCodSancionado($id_sancionado);
-        $sancion->setCodPartido($id_partido);
-        $sancion->setCodArbitro($id_arbitro);
-        $sancion->setCategoria($categoria);
-        $sancion->setDias_Penalizacion($dias);
-        $sancion->setFecha_Sancion($fecha_actual);
-        $sancion->setFecha_Fin($fecha_final);
-        $sancion->setEstado('Pendiente');
-        $sancion->setPrecio($precio);
-
-        $sancion->Crear_Sancion();
-
-
-   }
 
 
    
--- Modelos/Reportes.php
+++ Modelos/Reportes.php
@@ -1,5 +1,8 @@
 <?php
-class Reportes{
+require_once "database/Database.php";
+require_once "config/db_config.php";
+require_once "config/configGeneral.php";
+class Reportes extends Database{
     public $cod_reporte;
     public $cod_partido;
     public $ntarjetas;
@@ -57,8 +60,8 @@ class Reportes{
     }
     
     public function Actualizar_Reporte(){
-        $query = "UPDATE " . T_DETALLE. "SET(". REPORT_ID. ','. REPORT_PART. ','. REPORT_TARJ.','. REPORT_OBSERV.")" . 
-        " VALUES(:" . REPORT_ID . ", :" . REPORT_PART. ", :" .  REPORT_TARJ. ", :" . REPORT_OBSERV .  ") WHERE " . REPORT_ID . "= :" . REPORT_ID ;
+        $query = "UPDATE " . T_REPORTE. " SET(". REPORT_PART. ','. REPORT_TARJ.','. REPORT_OBSERV.")" . 
+        " VALUES(:" .  REPORT_PART. ", :" .  REPORT_TARJ. ", :" . REPORT_OBSERV .  ") WHERE " . REPORT_ID . "= :" . REPORT_ID ;
         $statement = $this->conexion->prepare($query);
         $statement->bindValue(':' . REPORT_ID,  $this->getIdReporte());
         $statement->bindValue(':' . REPORT_PART, $this->getIdPartido());
@@ -70,6 +73,24 @@ class Reportes{
         }
         return $message;
     }
+
+    public function Actualizar_Reporte_NumTarjetas(){
+        $query = "UPDATE " . T_REPORTE. " SET ".  REPORT_TARJ."=:" .  REPORT_TARJ. " WHERE " . REPORT_ID . "=:" . REPORT_ID ;
+        //$query = "UPDATE " . TBL_FACTURAS_CONF . " SET " . F_ESTADO . "=:" . F_ESTADO . " WHERE " . F_ID . "=:" . F_ID;
+        $statement = $this->conexion->prepare($query);
+        $statement->bindValue(':' . REPORT_ID,  $this->getIdReporte());
+        $statement->bindValue(':' . REPORT_TARJ, $this->getNumTarjetas());
+        echo $this->getIdReporte()."<br>";
+        echo $this->getNumTarjetas()."<br>";
+        var_dump($statement);
+        $message = "<h1>Error al actualizar estadio!</h1>";
+        if ($statement->execute()) {
+            $message = "<h1>Datos actualizados con éxito!</h1>";
+        }
+        return $message;
+    }
+
+    
     
     public function Ver_Reportes(){
         $query = "SELECT * FROM " . T_REPORTE;
